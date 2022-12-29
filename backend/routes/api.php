5b29adfca821ba49e9fd86551f36a6ad515448dd
Manimaran <manimaraan.r@gmail.com>
2022-12-29 23:00:01 +0530

    login and cros middleware added

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -2,16 +2,16 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\AuthController;
 
 Route::group([
 
-    'middleware' => 'api',
-    'prefix' => 'auth'
+    'middleware' => 'api'
 
 ], function () {
 
-    Route::post('login', 'AuthController@login');
+     Route::post('login',[AuthController::class,'login']);
+    // Route::post('login', 'AuthController@login');
     Route::post('logout', 'AuthController@logout');
     Route::post('refresh', 'AuthController@refresh');
     Route::post('me', 'AuthController@me');
