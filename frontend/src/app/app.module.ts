cb6ccf85b1c4d56a0a98d67701ce633cb6ac5411
Manimaran <manimaraan.r@gmail.com>
2022-12-29 19:55:01 +0530

    created components

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './component/navbar/navbar.component';
+import { LoginComponent } from './component/login/login.component';
+import { SignupComponent } from './component/signup/signup.component';
+import { ProfileComponent } from './component/profile/profile.component';
+import { RequestResetComponent } from './component/password/request-reset/request-reset.component';
+import { ResponseResetComponent } from './component/password/response-reset/response-reset.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    LoginComponent,
+    SignupComponent,
+    ProfileComponent,
+    RequestResetComponent,
+    ResponseResetComponent
   ],
   imports: [
     BrowserModule,
